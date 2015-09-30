67127c223199af5e33c4f0149abcb4223360898d
Jon Patterson <jon.mpatterson@gmail.com>
2015-09-29 22:39:47 -0600

    first commit

---
+++ supdawg.py
@@ -0,0 +1,23 @@
+import os
+import urllib
+
+import webapp2
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        template_values = {
+            'user_ip': os.environ["REMOTE_ADDR"],
+        }
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+], debug=True)
